9301d333221c346ec6d69bb36f97aff650c33d05
TruthDelta <bryansanches552@gmail.com>
2024-05-20 20:33:08 -0300

    sim

--- Aula5/script.js
+++ Aula5/script.js
@@ -4,8 +4,12 @@ artista: 'Pitty',
 album: 'Admiravél Chip Novo'
 };
 
+musica.nome = 'TROCANDO NOME DA MÚSICA';
+musica.artista = 'TROCANDO NOME DO(A) ARTISTA';
 musica.album = 'TROCANDO NOME DO ALBUM';
 
+console.log(musica.nome);
+console.log(musica.artista);
 console.log(musica.album);
 
 let listaDeBandas = [
